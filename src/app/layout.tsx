28ff8fd063ee6b5fe572250cec670e29461edf24
harinsd404 <harinsd168@gmail.com>
2026-05-06 10:03:46 +0900

    fix:: 저시력자 접근성을 위해 확대 제한 설정 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,6 @@ export const viewport: Viewport = {
   themeColor: '#000000',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export const metadata: Metadata = {
