c9f1971196b6b1e0c6a66e18e5ac178c5b8ec512
harinsd404 <harinsd168@gmail.com>
2026-05-12 08:32:10 +0900

    feat:: 로그 보관함 이동 url 변경

--- src/widgets/history-list/ui/HistoryList.tsx
+++ src/widgets/history-list/ui/HistoryList.tsx
@@ -22,24 +22,27 @@ export const HistoryList = ({ logs }: HistoryListProps) => {
 
   return (
     <div className="flex flex-col gap-3">
-      {logs.map((log, i) => (
-        <div
-          key={i}
-          className="flex flex-col gap-2 overflow-hidden rounded-[0.5rem] bg-gray-100 p-4"
-        >
-          <p className="text-[0.875rem] font-bold text-gray-800">
-            {dayjs(log.date).format('M월 D일')}의 기록
-          </p>
-          <LogCard authorName={log.nickname} time={`${log.hour}:00`} message={log.caption} />
-          <Link
-            href={`/log?date=${encodeURIComponent(log.date)}`}
-            aria-label={`${log.date} 로그 보기`}
-            className="text-left text-[0.625rem] tracking-[0.0125rem] text-gray-500"
+      {logs.map((log, i) => {
+        const dateParam = log.date.replace('-', '/');
+        return (
+          <div
+            key={i}
+            className="flex flex-col gap-2 overflow-hidden rounded-[0.5rem] bg-gray-100 p-4"
           >
-            바로가기 →
-          </Link>
-        </div>
-      ))}
+            <p className="text-[0.875rem] font-bold text-gray-800">
+              {dayjs(log.date).format('M월 D일')}의 기록
+            </p>
+            <LogCard authorName={log.nickname} time={`${log.hour}:00`} message={log.caption} />
+            <Link
+              href={`/log?date=${dateParam}`}
+              aria-label={`${log.date} 로그 보기`}
+              className="text-left text-[0.625rem] tracking-[0.0125rem] text-gray-500"
+            >
+              바로가기 →
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
