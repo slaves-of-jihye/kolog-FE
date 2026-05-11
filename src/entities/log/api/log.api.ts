e4fe723b1c0e3dd8f1ef54245ea626ad162c3304
harinsd404 <harinsd168@gmail.com>
2026-05-12 01:18:48 +0900

    feat:: 로그 공감 표시 API 구현

--- src/entities/log/api/log.api.ts
+++ src/entities/log/api/log.api.ts
@@ -1,12 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { baseApi } from '@/shared/api/base';
-import { HourlyLogResponse } from '../model/types';
+import { EmotionRequest, EmotionResponse, HourlyLogResponse } from '../model/types';
 
 export const logApi = {
   getHourlyLogs: async (hour?: number) => {
     const response = await baseApi.get<HourlyLogResponse>('/api/v1/logs/hour', {
       params: { hour },
     });
+    return response.data;
+  },
+  postEmotion: async (data: EmotionRequest) => {
+    const response = await baseApi.post<EmotionResponse>('/api/v1/video/emotion', data);
     return response.data;
   },
 };
@@ -16,5 +20,11 @@ export const useHourlyLogs = (hour?: number) => {
     queryKey: ['logs', 'hour', hour],
     queryFn: () => logApi.getHourlyLogs(hour),
     select: (response) => response.data,
+  });
+};
+
+export const useEmotionMutation = () => {
+  return useMutation({
+    mutationFn: (data: EmotionRequest) => logApi.postEmotion(data),
   });
 };
