d61ead77129a420777aed657f8f93243e9c01099
harinsd404 <harinsd168@gmail.com>
2026-07-19 20:16:33 +0900

    feat:: 로딩 추가

--- src/app/log/history/page.tsx
+++ src/app/log/history/page.tsx
@@ -7,13 +7,21 @@ import dayjs from 'dayjs';
 
 const History = () => {
   const today = dayjs().format('M-D');
-  const { data: logs } = useLogsByDate(today);
+  const { data: logs, isLoading } = useLogsByDate(today);
 
   return (
     <div className="relative min-h-screen w-full bg-white">
       <div className="flex flex-col gap-16 px-5 pt-[4.25rem] pb-16">
         <Header />
-        <HistoryList logs={logs} />
+        {isLoading ? (
+          <div className="flex flex-col gap-2 overflow-hidden rounded-[0.5rem] bg-gray-100 p-4">
+            <p className="text-[0.625rem] tracking-[0.0125rem] text-gray-600">
+              기록을 불러오는 중입니다...
+            </p>
+          </div>
+        ) : (
+          <HistoryList logs={logs} />
+        )}
       </div>
     </div>
   );
